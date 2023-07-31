c26290d1ba90ef5518a0adace366588ea0229033
Aleksei Semenov <alek753@yandex.ru>
2023-07-31 12:58:08 +0400

    refactoring scripts code

--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.brain_go import brain_go
+from brain_games.brain_engine import launch_game
 from brain_games.games import even
 
 
 def main():
-    brain_go(even)
+    launch_game(even)
 
 
 if __name__ == '__main__':
--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from brain_games.brain_go import brain_go
+from brain_games.brain_engine import launch_game
 from brain_games.games import progression
 
 
 def main():
-    brain_go(progression)
+    launch_game(progression)
 
 
 if __name__ == '__main__':
